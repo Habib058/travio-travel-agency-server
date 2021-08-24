c4c3c02e4a66846995a11be706bd435bf04b23b5
Habib058 <adnanshakil215@gmail.com>
2021-08-25 00:07:38 +0600

    done

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { MongoClient } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId;
 require('dotenv').config();
 const app = express()
-const port = 5000;
+const port =process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -110,4 +110,4 @@ app.get('/', (req, res) => {
   res.send('Hello Travio!')
 })
 
-app.listen(port || process.env.PORT)
+app.listen(port);
